3fc779746a3c3cd87e20b7fef49bf494625153e4
DoniyorShodiyevDoni <109288985+Doniyor2555@users.noreply.github.com>
2023-04-25 18:33:36 +0500

    Air-Bnb

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,4 +1,13 @@
+import { useState } from 'react';
 import Image from 'next/image';
+
+//Calendar Css imports
+
+import 'react-date-range/dist/styles.css'; // main style file
+import 'react-date-range/dist/theme/default.css'; // theme css file
+import { DateRangePicker } from 'react-date-range';
+
+//Calendar Css imports
 
 import {
   GlobeAltIcon,
@@ -7,39 +16,69 @@ import {
   UserCircleIcon,
 } from '@heroicons/react/outline'
 
+
 function Header() {
+  const [searchInput, setSearchInput] = useState("");
+  const [startDate, setStartDate] = useState(new Date());
+  const [endDate, setEndDate] = useState(new Date());
+
+  const handleChange = (ranges) => {
+    setStartDate(ranges.selection.startDate);
+    setEndDate(ranges.selection.endDate);
+  }
+
+  const selectionRange = {
+    startDate,
+    endDate,
+    key: 'selection',
+  }
+
   return (
     <header className='sticky top-0 z-50 grid grid-cols-3 bg-white shadow-md p-5 md:px-10'>
 
-        <div className="relative flex items-center h-10 cursor-pointer my-auto">
-          <Image src='https://links.papareact.com/qd3'
-            layout='fill'
-            objectFit='contain'
-            objectPosition='left'
-            alt='Logo'
+      <div className="relative flex items-center h-10 cursor-pointer my-auto">
+        <Image src='https://links.papareact.com/qd3'
+          layout='fill'
+          objectFit='contain'
+          objectPosition='left'
+          alt='Logo'
+        />
+      </div>
+
+      {/* Middle */}
+
+      <div className="flex items-center md:border-2 rounded-full py-2 md:shadow-sm">
+        <input
+          value={searchInput}
+          onChange={(e) => setSearchInput(e.target.value)}
+          type="text"
+          placeholder="Start your search"
+          className="flex-grow pl-5 bg-transparent outline-none text-sm text-gray-600 placeholder-gray-400" />
+        <SearchIcon className="hidden md:inline-flex h-8 bg-red-400 text-white rounded-full p-2 cursor-pointer md:mx-2" />
+      </div>
+
+      {/* Right */}
+
+      <div className="flex space-x-4 items-center justify-end text-gray-500">
+        <p className="hidden md:inline cursor-pointer">Become a host</p>
+        <GlobeAltIcon className='h-6 cursor-pointer' />
+
+        <div className="flex items-center space-x-2 border-2 rounded-full p-2">
+          <MenuIcon className="h-6" />
+          <UserCircleIcon className="h-6" />
+        </div>
+      </div>
+      {searchInput && (
+        <div className="flex flex-col col-span-3 mx-auto">
+          <DateRangePicker
+            ranges={[selectionRange]}
+            minDate={new Date()}
+            rangeColors={["#FD5B61"]}
+            onChange={handleChange}
           />
         </div>
-
-        {/* Middle */}
-
-        <div className="flex items-center md:border-2 rounded-full py-2 md:shadow-sm">
-          <input type="text" placeholder="Start your search" className="flex-grow pl-5 bg-transparent outline-none text-sm text-gray-600 placeholder-gray-400" />
-          <SearchIcon className="hidden md:inline-flex h-8 bg-red-400 text-white rounded-full p-2 cursor-pointer md:mx-2" />
-        </div>
-
-        {/* Right */}
-
-        <div className="flex space-x-4 items-center justify-end text-gray-500">
-          <p className="hidden md:inline cursor-pointer">Become a host</p>
-          <GlobeAltIcon className='h-6 cursor-pointer' />
-
-          <div className="flex items-center space-x-2 border-2 rounded-full p-2">
-            <MenuIcon className="h-6"/>
-            <UserCircleIcon className="h-6"/>
-          </div>
-        </div>
-
-      </header>
+      )}
+    </header>
 
   )
 }
