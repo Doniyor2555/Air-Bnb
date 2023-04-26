5bbc207cd2a30b1a1affa5a0f8fd8ad0934c0271
DoniyorShodiyevDoni <109288985+Doniyor2555@users.noreply.github.com>
2023-04-26 16:38:49 +0500

    Air-Bnb

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -10,7 +10,9 @@ const progress = new ProgressBar({
   delay: 100,
 });
 
-Router.events.on('routeChangeStart', progress.start)
+Router.events.on('routeChangeStart', progress.start);
+Router.events.on('routeChangeComplete', progress.finish);
+Router.events.on('routeChangeError', progress.finish);
 
 export default function App({ Component, pageProps }) {
   return <Component {...pageProps} />
